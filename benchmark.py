8f4704fd8962d4b801c85bfec0e0d504285115df
Claudio Capobianco <claudio.capobianco@istruzione.it>
2020-01-21 10:23:55 +0100

    Add default sort

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 from random import randrange
 from selection_sort import selection_sort
 from bubble_sort import bubble_sort
+from timeit import time
 
 # Creo la lista
 # Lista di partenza da ordinare
@@ -13,7 +14,13 @@ selection_sort_time = selection_sort(mylist.copy())
 bubble_sort_time = bubble_sort(mylist.copy())
 
 # Calcolo i tempi del sort di default di python....
-# ....
+newlist = mylist.copy()
+
+start_time = time.time()
+newlist.sort()
+stop_time = time.time()
+
+default_sort_time = stop_time-start_time
 
 # Salvo i risultati su file
 # ....
